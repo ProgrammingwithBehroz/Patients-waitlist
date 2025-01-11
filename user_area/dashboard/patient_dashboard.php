d9a4d1a809e9f4c32d63b8e8784a9df26bb2f134
Behroz <123737967+ProgrammingwithBehroz@users.noreply.github.com>
2025-01-11 19:58:58 +0430

    Update patient_dashboard.php

--- user_area/dashboard/patient_dashboard.php
+++ user_area/dashboard/patient_dashboard.php
@@ -82,7 +82,7 @@ session_start();
     <a href="index.html" class="logo d-flex align-items-center">
       <!-- Uncomment the line below if you also wish to use an image logo -->
       <!-- <img src="assets/img/logo.png" alt=""> -->
-      <h1 class="sitename">کلینیک درمانی ابلی سینا</h1>
+      <h1 class="sitename">کلینیک درمانی ابن سینا</h1>
     </a>
 
     <nav id="navmenu" class="navmenu">
